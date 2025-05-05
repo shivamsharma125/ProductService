43c4308ec9ebe46cc275c07f7771a54e615add5f
Shivam Sharma <shivamsharma8228@gmail.com>
2025-05-05 21:27:15 +0530

    JpaAuditing configuration moved from ProductServiceApplication to JpaAuditingConfig

---
+++ src/main/java/com/shivam/productservice/configs/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.shivam.productservice.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
